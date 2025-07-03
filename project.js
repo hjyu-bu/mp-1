009dcbfeaa447f70cd4676939524dd4dc034cabd
Hanjun Yoo <hjyu@bu.edu>
2025-07-03 17:42:27 -0400

    temporary-save-3

---
+++ project.js
@@ -0,0 +1,68 @@
+let val1=document.getElementById("input1");
+let val2=document.getElementById("input2");
+// Declare global and manipulate this based on the last button pushed
+let result=document.getElementById("output");
+
+function doadd(){
+    let value = Number(val1.value)+Number(val2.value);
+    if (value < 0) {
+        result.className = "neg-output";
+    }
+    else {
+        result.className = "pos-output";
+    }
+    result.innerHTML = `Result: ${value}`;
+}
+
+function dosub(){
+    let value = Number(val1.value)-Number(val2.value);
+    if (value < 0) {
+        result.className = "neg-output";
+    }
+    else {
+        result.className = "pos-output";
+    }
+    result.innerHTML = `Result: ${value}`;
+}
+
+function domult(){
+    let value = Number(val1.value)*Number(val2.value);
+    if (value < 0) {
+        result.className = "neg-output";
+    }
+    else {
+        result.className = "pos-output";
+    }
+    result.innerHTML = `Result: ${value}`;
+}
+
+function dodiv() {
+    let value = Number(val1.value)/Number(val2.value);
+    if (value < 0) {
+        result.className = "neg-output";
+    }
+    else {
+        result.className = "pos-output";
+    }
+    result.innerHTML = `Result: ${value}`;
+}
+
+function dopow() {
+    let value = 1;
+    for (let i = 0; i < Number(val2.value); i++) {
+        value *= Number(val1.value);
+    }
+    if (value < 0) {
+        result.className = "neg-output";
+    }
+    else {
+        result.className = "pos-output";
+    }
+    result.innerHTML = `Result: ${value}`;
+}
+
+function doclr(){
+    result.innerHTML = "";
+    val1.value="";
+    val2.value="";
+}
